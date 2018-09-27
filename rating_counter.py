c974e4ac8bf8a09cbde60f6f98771166b522ef11
yash upadhyay <yashup1997@gmail.com>
2018-09-28 01:01:38 +0530

    errors removed

--- rating_counter.py
+++ rating_counter.py
@@ -1,14 +1,14 @@
 from pyspark import SparkConf, SparkContext
 import collections
 
-conf = SparkConf.setMaster("local").setAppName("ratingsHistogram")
-sc = SparkContext(conf=conf)
+sc = SparkContext.getOrCreate(SparkConf().setMaster("local[*]"))
 
-num_lines = sc.text("ml-100k/u.data")
-ratings = lines.map(lambda x: x.split()[2])
+
+num_lines = sc.textFile("ml-100k/u.data")
+ratings = num_lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
 sortedResults = collections.OrderedDict(sorted(result.items()))
 
-for key, values in sortedResults.iteritems():
-	print("%s %i" % (key, value))
+for key, values in sortedResults.items():
+	print("%s %i" % (key, values))
