b3f9dae957914b34acae69672d5fd230b211ce01
yash upadhyay <yashup1997@gmail.com>
2018-09-28 00:47:42 +0530

    rating_counter.py edited

--- rating_counter.py
+++ rating_counter.py
@@ -8,4 +8,7 @@ num_lines = sc.text("ml-100k/u.data")
 ratings = lines.map(lambda x: x.split()[2])
 result = ratings.countByValue()
 
+sortedResults = collections.OrderedDict(sorted(result.items()))
 
+for key, values in sortedResults.iteritems():
+	print("%s %i" % (key, value))
