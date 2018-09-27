73877aa8f2c91f7a7cc85a6542281b48063e357a
yash upadhyay <yashup1997@gmail.com>
2018-09-28 00:44:16 +0530

    rating_counter.py edited

--- rating_counter.py
+++ rating_counter.py
@@ -4,4 +4,8 @@ import collections
 conf = SparkConf.setMaster("local").setAppName("ratingsHistogram")
 sc = SparkContext(conf=conf)
 
-num_lines = 
+num_lines = sc.text("ml-100k/u.data")
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue()
+
+
