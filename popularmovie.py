59e178510aea1e31629a416256751198de0bb8f5
yash upadhyay <yashup1997@gmail.com>
2018-09-28 20:31:27 +0530

    popularmovie.py edited

--- popularmovie.py
+++ popularmovie.py
@@ -2,3 +2,8 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster("local").setAppName("PopularMovies")
 sc = SparkContext(conf = conf)
+
+lines = sc.textFile("ml-100k/u.data")
+movies = lines.map(lambda x:(int(x.split()[1]),1))
+moviesCount = movies.reduceByKey(lambda x,y :x+y)
+flipped = moviesCount.map(lambda xy: (xy[1],xy[0]))
