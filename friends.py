08eb859311b8102083388cd77dfc60fe364dbd83
yash upadhyay <yashup1997@gmail.com>
2018-09-28 11:20:19 +0530

    parse line function added

--- friends.py
+++ friends.py
@@ -2,3 +2,9 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster("local").setAppName("FriendsByAge")
 sc  = SparkContext(conf = conf )
+
+def parseLine(line):
+    fields = line.split(',')
+    age = int(fields[2])
+    numFriends = int(fields[3])
+    return (age,numFriends)
