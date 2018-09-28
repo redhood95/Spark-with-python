21f981fc05a100775e5eacb97a8519994ba20a64
yash upadhyay <yashup1997@gmail.com>
2018-09-28 11:35:42 +0530

    friends.py edited

--- friends.py
+++ friends.py
@@ -13,8 +13,8 @@ def parseLine(line):
 lines = sc.textFile("fakefriends.csv")
 
 rdd = lines.map(parseLine)
-totalByAge = rdd.mapValues(lambda x: (x,1)).reduceBykey(lambda x,y : (x[0] + y[0], x[1] +y[1]))
-averagesByAge = totalsByAge.mapValues(lambda x:x[0]/x[1])
+totalByAge = rdd.mapValues(lambda x: (x,1)).reduceByKey(lambda x,y : (x[0] + y[0], x[1] +y[1]))
+averagesByAge = totalByAge.mapValues(lambda x:x[0]/x[1])
 results = averagesByAge.collect()
 for result in results:
     print(result)
