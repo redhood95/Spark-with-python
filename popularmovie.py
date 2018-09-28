214db08eb360161a84f91ab5d57ad7009fafb76a
yash upadhyay <yashup1997@gmail.com>
2018-09-28 20:36:03 +0530

    popularmovie.py edited

--- popularmovie.py
+++ popularmovie.py
@@ -7,3 +7,9 @@ lines = sc.textFile("ml-100k/u.data")
 movies = lines.map(lambda x:(int(x.split()[1]),1))
 moviesCount = movies.reduceByKey(lambda x,y :x+y)
 flipped = moviesCount.map(lambda xy: (xy[1],xy[0]))
+sortedMovies = flipped.sortByKey()
+
+results = sortedMovies.collect()
+
+for result in results:
+    print(result)
