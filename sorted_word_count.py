d3ccdad08253719e32cf2aa78dd0818555485919
yash upadhyay <yashup1997@gmail.com>
2018-09-28 19:52:31 +0530

    sorted_word_count.py edited

--- sorted_word_count.py
+++ sorted_word_count.py
@@ -5,3 +5,11 @@ def normalizeWords(text):
     return  re.compile(r'\w+',re.UNICODE).split(text.lower())
 
 conf = SparkConf().setMaster("local").setAppName("wordCounts")
+
+sc = SparkContext(conf = conf)
+
+input = sc.textFile("book.txt")
+words = input.flatMap(normalizeWords)
+wordCount = words.map(lambda x:(x,1)).reduceByKey(lambda x, y : x+y)
+wordCountSorted= wordCount.map(lambda x:(x[1],x[0])).sortByKey()
+results = wordCountSorted.collect()
