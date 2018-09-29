d3db0635ec863b9601b3b9767d41c2698cdccb25
yash upadhyay <yashup1997@gmail.com>
2018-09-29 15:35:37 +0530

    edited

--- most_popular_superhero.py
+++ most_popular_superhero.py
@@ -11,4 +11,15 @@ def parseName(line):
     fields = line.split('\"')
     return (int(fields[0], fields[1].encode("utf8")))
 
-names =  sc.textFile("marvel-names.txt")
+names =  sc.textFile("data\marvel-names.txt")
+
+namesRdd = names.map(parseName)
+lines = sc.textFile("data\marvel-graph.txt")
+pairing = lines.map(countCoOccurrence)
+
+totalFriendsbyCharacter = pairing.reduceByKey(lambda x,y : x+y)
+flipped = totalFriendsbyCharacter.map(lambda xy : (xy[1], xy[0]))
+
+mostPopular = flipped.max()
+
+mostPopularName = namesRdd.lookup(mostPopular[1])[0]
