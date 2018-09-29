3e44930870579d7d5f06bd1e470c87f03d8f3e9a
yash upadhyay <yashup1997@gmail.com>
2018-09-29 15:30:56 +0530

    helper funtions added

--- most_popular_superhero.py
+++ most_popular_superhero.py
@@ -3,4 +3,12 @@ from pyspark import SparkConf , SparkContext
 conf = SparkConf().setMaster("local").setAppName("PopularSuperhero")
 sc = SparkContext(conf = conf)
 
+def countCoOccurrence(line):
+    elements = line.split()
+    return (int(element[0]), len(elements) -1)
+
+def parseName(line):
+    fields = line.split('\"')
+    return (int(fields[0], fields[1].encode("utf8")))
+
 names =  sc.textFile("marvel-names.txt")
