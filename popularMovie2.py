281cc9115cc982c95ff917f8de81107649b028e7
yash upadhyay <yashup1997@gmail.com>
2018-09-29 15:03:13 +0530

    loadMovieNames function added

--- popularMovie2.py
+++ popularMovie2.py
@@ -2,3 +2,12 @@ from spark import SparkConf , SparkContext
 
 conf = SparkConf().setMaster("local").setAppName("PopularMovies")
 sc = SparkContext(conf = conf)
+
+
+def loadMovieNames():
+    movieNames = {}
+    with open("ml-100k/u.item") as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
