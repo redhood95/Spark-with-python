ec26a45c91576e25078d3c8663c50e1556d821a6
yash upadhyay <yashup1997@gmail.com>
2018-10-01 00:38:12 +0530

    filterDuplicates function added

--- movie-similarity.py
+++ movie-similarity.py
@@ -16,3 +16,9 @@ def makePairs( userRatings ):
     (movie1, rating1) = ratings[0]
     (movie2, rating2) = ratings[1]
     return ((movie1, movie2), (rating1, rating2))
+
+def filterDuplicates( userRatings ):
+    ratings = userRatings[1]
+    (movie1, rating1) = ratings[0]
+    (movie2, rating2) = ratings[1]
+    return movie1 < movie2
