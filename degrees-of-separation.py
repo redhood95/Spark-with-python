238aa232ebdb588517529b839726afcb5e9c6e90
yash upadhyay <yashup1997@gmail.com>
2018-09-30 23:56:44 +0530

    convert to bfs function added

--- degrees-of-separation.py
+++ degrees-of-separation.py
@@ -2,3 +2,24 @@ from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setMaster("local").setAppName("DegreesOfSeparation")
 sc = SparkContext(conf = conf)
+
+startCharacterID = 5306 #spiderman
+targetCharcterID = 14 #Adam
+
+hitCounter = sc.accumulator(0)
+
+def convertToBFS(line):
+    fields=line.split()
+    heroID = int(fields[0])
+    connections = []
+    for connections in fields[1:]:
+        connections.append(int(connections))
+
+    color = 'white'
+    distance = 9999
+
+    if  (heroID == startCharacterID):
+        color = 'GRAY'
+        distance = 0
+
+    return (heroID, (connetions, distance, color))
