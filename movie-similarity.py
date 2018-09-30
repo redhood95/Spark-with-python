9d185b5fe11fbd98b0606abef6ede099b2afb38c
yash upadhyay <yashup1997@gmail.com>
2018-10-01 00:31:29 +0530

    loadMovieNmes function added

--- movie-similarity.py
+++ movie-similarity.py
@@ -1,3 +1,12 @@
 import sys
 from pyspark import SparkConf, SparkContext
 from math import sqrt
+
+
+def loadMovieNames():
+    movieNames = {}
+    with open("ml-100k/u.ITEM", encoding='ascii', errors='ignore') as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
